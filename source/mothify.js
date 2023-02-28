236d04081a6f800df2a8899d3fba3c5ce9f00716
beep-beep-beep-boop <79901832+beep-beep-beep-boop@users.noreply.github.com>
2023-02-27 19:43:37 -0700

    a

--- source/mothify.js
+++ source/mothify.js
@@ -31,18 +31,29 @@ function addStyle(css_str) {
 const background_color = "rgb(28, 27, 34)";
 const width = "720px";
 const font_family = "Georgia, \"Times New Roman\", serif"
-const font_size = "24px"
-const line_height = "1.6"
+const font_size_pixels = 24;
+const line_height_pixels = 20;
 const text_color_default = "#eee";
 const text_color_1 = "rgb(158, 141, 252)";
 const text_color_2 = "rgb(88, 171, 255)";
 
+const ratio = 128 / (new_line_height * 4)
+const new_bg_size = 128 / ratio;
+const new_bg_pos = 0;//8 / ratio;
+
 /// the style that helps put the grandient lines
 const moth_style = `
+p {
+    font-size:${font_size_pixels}px;
+    line-height:${line_height_pixels}px;
+    background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAABgCAIAAAC46DQiAAAAYUlEQVQoz73SwQ3DIBAF0aetYDuAEtNJWoQKTAfOgcgS+M5pVnMafS0+X4JWHyAv4CbmdQDJICrthULfpYuckU52DshoUDd0KKuUZizc5zpJRlBpLxT6KtdFT3aO3H/wjx/oyySm/xnbdAAAAABJRU5ErkJggg==);
+    background-position-y:${new_bg_pos}px;
+    background-size:100% ${new_bg_size}px;
+    -webkit-background-clip:text;
+    -webkit-text-fill-color:transparent
+}
 body {
     font-family: ${font_family};
-    font-size: ${font_size};
-    line-height: ${line_height};
     background-color: ${background_color};
 }
 .page {
@@ -54,76 +65,51 @@ body {
 .page .line {
     text-align: justify; 
 }
-.page .line {
-    color: transparent;
-    -webkit-background-clip: text;
-    background-clip: text;
-}
-.page .line + srword:not(.line),
-.page .line srword:first-child {
-    white-space: nowrap; 
-}
-
-.page .line:nth-child(4n + 1) {
-    background-image: -webkit-linear-gradient(to right, ${text_color_default} 0%,${text_color_1} 80%);
-    background-image: linear-gradient(to right, ${text_color_default} 0%,${text_color_1} 80%);
-}
-.page .line:nth-child(4n + 2) {
-    background-image: -webkit-linear-gradient(to right, ${text_color_1} 20%,${text_color_default} 100%);
-    background-image: linear-gradient(to right, ${text_color_1} 20%,${text_color_default} 100%);
-}
-.page .line:nth-child(4n + 3) {
-    background-image: -webkit-linear-gradient(to right, ${text_color_default} 0%,${text_color_2} 80%);
-    background-image: linear-gradient(to right, ${text_color_default} 0%,${text_color_2} 80%);
-}
-.page .line:nth-child(4n + 4) {
-    background-image: -webkit-linear-gradient(to right, ${text_color_2} 20%,${text_color_default} 100%);
-    background-image: linear-gradient(to right, ${text_color_2} 20%,${text_color_default} 100%);
-}
 `;
 
 function mothify() {
+    console.log("meow");
     // replace document with readable document
-    document.body.innerHTML = readable();
+    //document.body.innerHTML = readable();
 
     clearStyles();
 
     addStyle(moth_style);
-
-    (function () {
-
-        $('.page p, ul, ol').each(function () {
-            var obj = $(this);
-            var html = obj.html().replace(/(?<!(<\/?[^>]*|&[^;]*))([^\s<]+)/g, '$1<word>$2 </word>');
-            obj.html(html);
-        });
-
-        var groups = 4;
-        var spans = $('.page word');
-
-        function highlight() {
-            var startOfGroup = 0;
-            $('.page .line').find('word:first').unwrap();
-
-            for (var i = 1; i <= spans.length; i++) {
-                if (i != spans.length &&
-                    spans[i - 1].offsetTop === spans[i].offsetTop &&
-                    spans[i - 1].offsetHeight === spans[i].offsetHeight
-                ) continue;
-                spans.slice(startOfGroup, i).wrapAll(`<span class='line'></span>`);
-                startOfGroup = i;
-            }
-        }
-
-        let timeout;
-        $(window).on('load resize', function () {
-            window.clearTimeout(timeout);
-            timeout = window.setTimeout(highlight, 30);
-        });
-
-        highlight();
-
-    })();
+//
+//    (function () {
+//
+//        $('.page p, ul, ol').each(function () {
+//            var obj = $(this);
+//            var html = obj.html().replace(/(?<!(<\/?[^>]*|&[^;]*))([^\s<]+)/g, '$1<word>$2 </word>');
+//            obj.html(html);
+//        });
+//
+//        var groups = 4;
+//        var spans = $('.page word');
+//
+//        function highlight() {
+//            var startOfGroup = 0;
+//            $('.page .line').find('word:first').unwrap();
+//
+//            for (var i = 1; i <= spans.length; i++) {
+//                if (i != spans.length &&
+//                    spans[i - 1].offsetTop === spans[i].offsetTop &&
+//                    spans[i - 1].offsetHeight === spans[i].offsetHeight
+//                ) continue;
+//                spans.slice(startOfGroup, i).wrapAll(`<span class='line'></span>`);
+//                startOfGroup = i;
+//            }
+//        }
+//
+//        let timeout;
+//        $(window).on('load resize', function () {
+//            window.clearTimeout(timeout);
+//            timeout = window.setTimeout(highlight, 30);
+//        });
+//
+//        highlight();
+//
+//    })();
 
 }
 
