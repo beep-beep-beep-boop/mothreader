e8571dc08091a409037736add219474a10a11c03
beep-beep-beep-boop <79901832+beep-beep-beep-boop@users.noreply.github.com>
2023-03-11 01:44:52 -0700

    add function for generating custom image gradient

---
+++ source/gradient.js
@@ -0,0 +1,53 @@
+import chroma from "chroma-js";
+
+const gradient_length = 10;
+const between_length = 14;
+const shift_amount = 6;
+
+function gradient (from, to) {
+    return chroma.scale([from, to]).colors(gradient_length);
+}
+
+function strip (color) {
+    const a = new Array(between_length);
+    return a.fill(color);
+}
+
+function shift (a) {
+
+    const before_arr = a.slice(-shift_amount);
+    const after_arr = a.slice(0, a.length - shift_amount)
+
+    const res = [...before_arr, ...after_arr];
+
+    return res;
+}
+
+function moth_grad_array (color_1, color_fill, color_2) {
+    const line_1 = shift([...strip(color_1), ...gradient(color_1, color_fill), ...strip(color_fill), ...gradient(color_fill, color_2), ...strip(color_2), ...gradient(color_2, color_fill), ...strip(color_fill), ...gradient(color_fill, color_1)]);
+    const line_2 = shift([...strip(color_fill), ...gradient(color_fill, color_1), ...strip(color_1), ...gradient(color_1, color_fill), ...strip(color_fill), ...gradient(color_fill, color_2), ...strip(color_2), ...gradient(color_2, color_fill)]);
+    return [line_1, line_2];
+}
+
+function gen_grad_str(color_1, color_fill, color_2) {
+    const a = moth_grad_array(color_1, color_fill, color_2)
+
+    let canvas = document.querySelector("canvas"), // Select our canvas element
+        ctx = canvas.getContext("2d"), // Save the context we're going to use
+        width = a[0].length, // Get the width
+        height = a.length, // Get the height
+        scale = 10; // Scales the whole image by this amount
+
+    canvas.width = width * scale; // Set the canvas width
+    canvas.height = height * scale; // Set the canvas height
+
+    // Loop through each color and draw that section
+    for(var row = 0; row < height; row++) {
+        for(var col = 0; col < width; col++) {
+            ctx.fillStyle = a[row][col]; // Set the color to the one specified
+            ctx.fillRect(col * scale, row * scale, scale, scale); // Actually draw the rectangle
+        }
+    }
+
+    return canvas.toDataURL();
+}
