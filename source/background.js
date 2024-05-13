916e9a2e86d04f44f54d302ce2cbecb22f0e0bee
hello <79901832+beep-beep-beep-boop@users.noreply.github.com>
2024-05-13 12:56:15 -0600

    update to manifest v3

--- source/background.js
+++ source/background.js
@@ -2,6 +2,7 @@
 import './options-storage.js';
 
 // tell the content script when the user clicks the extension icon
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
     chrome.tabs.sendMessage(tab.id, "mothreader");
+    // Send a message to the active tab
 });
