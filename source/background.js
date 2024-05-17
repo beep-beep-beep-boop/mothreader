d5a5e691ba283284d300d78ec6306dd161155703
hello <79901832+beep-beep-beep-boop@users.noreply.github.com>
2024-05-17 01:09:30 -0600

    only inject content script into tabs when we need to

--- source/background.js
+++ source/background.js
@@ -3,6 +3,16 @@ import './options-storage.js';
 
 // tell the content script when the user clicks the extension icon
 chrome.action.onClicked.addListener(function (tab) {
-    chrome.tabs.sendMessage(tab.id, "mothreader");
+    // this will only be picked up by tabs that already have the script,
+    // so that means the user is toggling the coloring off.
+    chrome.tabs.sendMessage(tab.id, "unmothify")
+        .catch(e => {}); // the error is expected when the content script isn't loaded so we can ignore it.
+
+    //chrome.tabs.sendMessage(tab.id, "mothreader");
     // Send a message to the active tab
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ["content.js"]
+    });
+
 });
--- source/content.js
+++ source/content.js
@@ -1,25 +1,22 @@
 import optionsStorage from './options-storage.js';
 import mothify from './mothify';
 
-async function init() {
-	const options = await optionsStorage.getAll();
-	if (options.always_moth === true) {
-		mothify();
-	}
-}
+console.log("meow");
+mothify();
 
-init();
-
-let already_mothed = false;
-
+//async function init() {
+//	const options = await optionsStorage.getAll();
+//	if (options.always_moth === true) {
+//		mothify();
+//	}
+//}
+//
+//init();
+//
+//let already_mothed = false;
+//
 chrome.runtime.onMessage.addListener(function (msg) {
-	if (msg === "mothreader") {
-		if (!already_mothed) {
-			already_mothed = true;
-			mothify();
-		} else {
-			// reload to get rid of the moth
+	if (msg === "unmothify") {
 			location.reload();
-		}
 	}
 });
